723f68a528974bdf6591f7712cb2f84fdb53e35c
Silvia Lin Lü <ssll@kth.se>
2026-05-22 22:23:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # website
+
+Command to start Docker:
+docker run -it --rm `
+>> -v "C:\Users\laira\Documents\GitHub\portfolio-website:/app" `
+>> -w /app `
+>> --entrypoint sh node:24-slim
+
+  
---
+++ src/your_info.jsx
@@ -0,0 +1,191 @@
+//////////////////////////////////////// ** HEADER / HERO SECTION **///////////////////////////////////////////
+
+// your_info.jsx
+
+const backgroundImageUrl = 'https://images.wallpaperscraft.com/image/single/man_photographer_roof_146991_3840x2400.jpg';
+
+
+// Enter here your first and last name
+const name = {
+  firstname: 'laira',
+  // lastname: 'Dominion',
+// Enter as url a link where your resume can be downloaded ( dropbox, OneDrive, ect )
+  // url: 'https://www.google.com',
+};
+
+// Display your job title or skills or whatever you want in the typewriter
+const typeWriterText = [
+  'Cybersecurity',
+  'Information Technology',
+  'Forensics'  
+];
+
+// Social media profiles buttons
+const socialProfiles = [
+  {
+    name: 'LinkedIn',
+    icon: 'lni lni-linkedin',
+    url: 'https://www.linkedin.com/in/silvialinlu',
+  },
+  {
+    name: 'GitHub',
+    icon: 'lni lni-github',
+    url: 'https://github.com/Silv-ia',
+  },
+  //   Add more social profiles here, it will automatically add more link-buttons with icons (if available)
+  //   {
+  //     name: 'Twitter',
+  //     icon: 'lni lni-twitter',
+  //     url: 'https://twitter.com/your-username',
+  //   },
+  //   {
+  //     name: 'Facebook',
+  //     icon: 'lni lni-facebook',
+  //     url: 'https://facebook.com/your-username',
+  //   },
+];
+
+//////////////////////////////////////// ** CONTENT SECTION **//////////////////////////////////////////////////
+
+
+const categories = [
+  {
+    name: 'about',
+    icon: 'lni lni-code',
+    title: 'Photography',
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-camera',
+        title: 'HTML5',
+      },
+      {
+        icon: 'lni lni-certificate',
+        title: 'CSS3',
+      },
+      {
+        icon: 'lni lni-instagram',
+        title: 'JavaScript',
+      },
+    ],
+  },
+  {
+    name: 'skills',
+    icon: 'lni lni-code',
+    title: 'Software',
+    description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-adobe',
+        title: 'adobe',
+      },
+      {
+        icon: 'lni lni-code',
+        title: 'Code',
+      },
+      {
+        icon: 'lni lni-sketch',
+        title: 'Sketch'
+      },
+     
+      
+    ],
+  },
+  {
+    name: 'projects',
+    icon: 'lni lni-code',
+    title: 'Brands',
+    description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas diam lorem, tempus at dapibus vitae, vehicula sit amet dui. Fusce at libero id massa ornare molestie sed eu tellus.",
+    skills: [
+      {
+        icon: 'lni lni-pinterest',
+        title: 'Pinterest',
+      },
+      {
+        icon: 'lni lni-crop',
+        title: 'Crop',
+      },
+      
+    ],
+  },
+  
+];
+
+// Here you can give in your achiements in a number counter animation
+const achievements = [
+  { word: 'Bachelor\'s degree', value: 100, unit: '%' },
+  { word: 'Master\'s degree', value: 50, unit: '%' },
+  { word: 'experience', value: 20, unit: ' years' },
+];
+
+// Languages?
+const languages = [
+  { word: 'English', value: 100, unit: '%' },
+  { word: 'Swedish', value: 100, unit: '%' },
+  {word: 'Chinese', value: 50, unit: '%' },
+  { word: 'Spanish', value: 75, unit: ' %' },
+];
+
+
+// If you already have some projects, fill the url 
+const projectData = [
+  {
+    title: 'Project 1',
+    description: 'Description of Project 1',
+    demoUrl: 'https://wallpaperaccess.com/full/530366.jpg' 
+  },
+  {
+    title: 'Project 2',
+    description: 'Description of Project 2',
+    demoUrl: 'https://wallpapercave.com/wp/wp1933134.jpg' 
+  },
+  {
+    title: 'Project 3',
+    description: 'Description of Project 3',
+    demoUrl: 'https://c4.wallpaperflare.com/wallpaper/43/399/700/photography-camera-birds-animals-wallpaper-preview.jpg' 
+  },
+  {
+    title: 'Project 2',
+    description: 'Description of Project 2',
+    demoUrl: 'src/assets/images/Jonathan Dominion Template.gif' 
+  },
+  
+];
+
+
+//////////////////////////////////////// ** FOOTER SECTION **//////////////////////////////////////////////////
+
+
+
+// You can tell something about yourself in the infotext.
+const aboutMeText = {
+  infotext: `Hi, I'm laira. 
+    `,
+  power_slogan: ``,
+};
+
+// For contact form: You need to make an account on emailjs.com
+// There you can choose a free tier ( 200 emails per month )
+// In your account settings you can see 'serviceID, templateID and userID. 
+// Fill them here and it will automatically work. 
+
+const emailConfig = {
+  serviceID: 'service_XXXXXXX',
+  templateID: 'template_XXXXXXXX',
+  userID: 'XXXXXXXXXXXX',
+};
+
+export {
+  backgroundImageUrl,
+  name,
+  typeWriterText,
+  socialProfiles,
+  categories,
+  achievements,
+  projectData,
+  aboutMeText,
+  emailConfig,
+};
