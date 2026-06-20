62c926d480358e110f80cacd29f6f29abd86ede9
Silvia Lin Lü <ssll@kth.se>
2026-06-20 17:00:43 +0200

    Update Hero.jsx

--- src/components/1. Header Components/Hero/Hero.jsx
+++ src/components/1. Header Components/Hero/Hero.jsx
@@ -58,7 +58,7 @@ const Hero = () => {
           </div>
 
           <div className="terminal-body">
-            <p>guest@laira:~$ .\whoami</p>
+            <p>guest@laira:~$ whoami.txt</p>
             <p>Hi, I'm {firstname}. I'm an MSc student in Cybersecurity, with a BSc in Information Technology. I am curious about forensics and offensive security. Here you can see my projects and achievements! 
             </p>
 
