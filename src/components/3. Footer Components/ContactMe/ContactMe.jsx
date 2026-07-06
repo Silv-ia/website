496a0633ff02fe188e8e30c35b81cf549ad5bc55
Silvia Lin Lü <ssll@kth.se>
2026-07-06 14:09:27 +0200

    contact

--- src/components/3. Footer Components/ContactMe/ContactMe.jsx
+++ src/components/3. Footer Components/ContactMe/ContactMe.jsx
@@ -26,56 +26,40 @@ const ContactMe = () => {
 
   return (
     <section id='ContactMe'>
-      <div className='form-container wow fadeInRight' data-wow-delay='.4s'>
-        <div className='contact-form-wrapper d-flex justify-content-center'>
-          <form ref={form} onSubmit={sendEmail} className='contact-form'>
-            <h5 className='title'>Contact Me</h5>
-            <p className='description'>
-              Feel free to contact me if you like to reach out or have any
-              questions.
-            </p>
-            <div>
-              <input
-                type='text'
-                className='form-control rounded border-white mb-3 form-input'
-                name='user_name'
-                placeholder='Name'
-                required
-              />
-            </div>
-            <div>
-              <input
-                type='email'
-                className='form-control rounded border-white mb-3 form-input'
-                name='user_email'
-                placeholder='Email'
-                required
-              />
-            </div>
-            <div>
-              <textarea
-                className='form-control rounded border-white mb-3 form-text-area'
-                name='message'
-                rows='5'
-                cols='30'
-                placeholder='Message'
-                required
-              ></textarea>
-            </div>
-            <div className='submit-button-wrapper' data-wow-delay='.6s'>
-              {!isMessageSent ? (
-                <button type='submit' className='submit-button'>
-                  Send
-                </button>
-              ) : (
-                <div className='success-message'>
-                  <h4>Message Sent Successfully!</h4>
-                  <p>Thank you for contacting me.</p>
-                  
-                </div>
-              )}
-            </div>
-          </form>
+      <div className='section-container'>
+
+        
+        <div className='form-container wow fadeInRight' data-wow-delay='.4s'>
+          <div className='contact-form-wrapper d-flex justify-content-center'>
+            <form ref={form} onSubmit={sendEmail} className='contact-form'>
+              <h2 className='title'>Contact Me</h2>
+              <div>
+                <input
+                  type="email"
+                  name="user_email"
+                  placeholder="Enter your email..."
+                />
+
+                <textarea
+                  name="message"
+                  placeholder="Type your message here..."
+                />
+              </div>
+              <div className='submit-button-wrapper' data-wow-delay='.6s'>
+                {!isMessageSent ? (
+                  <button type='submit' className='submit-button'>
+                    Send
+                  </button>
+                ) : (
+                  <div className='success-message'>
+                    <h4>Message Sent Successfully!</h4>
+                    <p>Thank you for contacting me.</p>
+                    
+                  </div>
+                )}
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </section>
