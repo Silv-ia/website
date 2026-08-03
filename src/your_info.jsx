552b6c5643b1d0dbace6ddf8735b91e30815e938
Silvia Lin Lü <ssll@kth.se>
2026-08-03 17:11:32 +0200

    small tweak

--- src/your_info.jsx
+++ src/your_info.jsx
@@ -210,8 +210,8 @@ const categories = [
 // Here you can give in your achiements in a number counter animation
 const achievements = [
   {word: 'Exchange semester', value: 'Technische Universität Wien', unit: '2027 • Wien, Austria' },
-  {word: 'Summer School', value: 'ICSSS: NCIA, Europol, Leiden Univ., Security Delta', unit: '2026 • Den Haag, Netherlands' },
   {word: 'M.Sc. Cybersecurity', value: 'KTH Royal Institute of Technology', unit: '2027 • Stockholm, Sweden' },
+  {word: 'Summer School', value: 'ICSSS: NCIA, Europol, Leiden University, Security Delta', unit: '2026 • Den Haag, Netherlands' },
   {word: 'B.Sc. Information Technology', value: 'KTH Royal Institute of Technology', unit: '2026 • Stockholm, Sweden' },
   {word: 'High School Diploma', value: 'ABB Industrigymnasium (Hitachigymnasiet)', unit: '2021 • Västerås, Sweden'},
   {word: 'Changemaker Camp', value: 'Raoul Wallenberg Academy, Chefakademin', unit: '2020 • Stockholm, Sweden'},
